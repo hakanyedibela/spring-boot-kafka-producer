c5f50eba3192323356fc31274d2b0f774fc8af72
Hakan Yedibela <hakan.yedibela@gmail.com>
2022-04-10 13:34:09 +0200

    Initial commit

---
+++ src/main/java/ki/dev/main/ComplexKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package ki.dev.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ki/dev/main/ComplexKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package ki.dev.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
